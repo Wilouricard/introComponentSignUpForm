f52b8e6935f359346bec1f7f02106f512fba753e
Nam Tang <nam.tang@live.fr>
2022-11-07 11:59:39 +0100

    error msg & icon removed

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,34 +1,34 @@
 import React from "react";
 
 export default function Form() {
-  const errorMsg = document.querySelector("#error-msg");
-  const errorIcon = document.querySelector("#error-icon");
-  const errorMsg2 = document.querySelector("#error-msg2");
-  const errorIcon2 = document.querySelector("#error-icon2");
-  const errorMsg3 = document.querySelector("#error-msg3");
-  const errorIcon3 = document.querySelector("#error-icon3");
-  const errorMsg4 = document.querySelector("#error-msg4");
-  const errorIcon4 = document.querySelector("#error-icon4");
-  const input = document.querySelector("#input");
-  const input2 = document.querySelector("#input2");
-  const input3 = document.querySelector("#input3");
-  const input4 = document.querySelector("#input4");
-  const submitButton = document.getElementById("submit");
+  // const errorMsg = document.querySelector("#error-msg");
+  // const errorIcon = document.querySelector("#error-icon");
+  // const errorMsg2 = document.querySelector("#error-msg2");
+  // const errorIcon2 = document.querySelector("#error-icon2");
+  // const errorMsg3 = document.querySelector("#error-msg3");
+  // const errorIcon3 = document.querySelector("#error-icon3");
+  // const errorMsg4 = document.querySelector("#error-msg4");
+  // const errorIcon4 = document.querySelector("#error-icon4");
+  // const input = document.querySelector("#input");
+  // const input2 = document.querySelector("#input2");
+  // const input3 = document.querySelector("#input3");
+  // const input4 = document.querySelector("#input4");
+  // const submitButton = document.getElementById("submit");
 
-  submitButton.addEventListener("click", () => {
-    errorMsg.classList.remove("hidden");
-    errorIcon.classList.remove("hidden");
-    errorMsg2.classList.remove("hidden");
-    errorIcon2.classList.remove("hidden");
-    errorMsg3.classList.remove("hidden");
-    errorIcon3.classList.remove("hidden");
-    errorMsg4.classList.remove("hidden");
-    errorIcon4.classList.remove("hidden");
-    input.classList.remove("mb-4");
-    input2.classList.remove("mb-4");
-    input3.classList.remove("mb-4");
-    input4.classList.remove("mb-4");
-  });
+  // submitButton.addEventListener("click", () => {
+  //   errorMsg.classList.remove("hidden");
+  //   errorIcon.classList.remove("hidden");
+  //   errorMsg2.classList.remove("hidden");
+  //   errorIcon2.classList.remove("hidden");
+  //   errorMsg3.classList.remove("hidden");
+  //   errorIcon3.classList.remove("hidden");
+  //   errorMsg4.classList.remove("hidden");
+  //   errorIcon4.classList.remove("hidden");
+  //   input.classList.remove("mb-4");
+  //   input2.classList.remove("mb-4");
+  //   input3.classList.remove("mb-4");
+  //   input4.classList.remove("mb-4");
+  // });
 
   return (
     <div className="pt-8 shadow-xl relative z-10">
@@ -109,7 +109,10 @@ export default function Form() {
             className=" translate-x-60 -translate-y-14 hidden md:translate-x-[500px]"
           />
         </i>
-        <button className="uppercase bg-green py-3 text-white tracking-widest rounded-lg shadow-inner" id="submit">
+        <button
+          className="uppercase bg-green py-3 text-white tracking-widest rounded-lg shadow-inner"
+          id="submit"
+        >
           Claim your free trial
         </button>
         <p className="text-center text-[11px] px-5 text-grayish-Blue leading-4">
--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Text() {
   return (
     <div className="text-center pb-12">
-      <img src="public\images\bg-intro-mobile.png" alt="" className="absolute z-0 -translate-y-32 -translate-x-6 opacity-70 md:hidden"/>
-      <img src="public\images\bg-intro-desktop.png" alt="" className="md:absolute z-0 md:opacity-70 hidden md:inline md:-translate-y-10 md:-translate-x-[330px]"/>
+      <img src="\images\bg-intro-mobile.png" alt="" className="absolute z-0 -translate-y-32 -translate-x-6 opacity-70 md:hidden"/>
+      <img src="\images\bg-intro-desktop.png" alt="" className="md:absolute z-0 md:opacity-70 hidden md:inline md:-translate-y-10 md:-translate-x-[330px]"/>
       <h1 className=" text-white text-3xl font-bold pb-10 relative z-10 md:text-5xl md:pr-20 md:pl-32 md:flex md:pt-44 md:text-start">Learn to code by watching others</h1>
       <p className="text-white relative z-10 md:text-start md:pl-32 md:pr-20">
         See how experienced developers solve problems in real-time. Watching
